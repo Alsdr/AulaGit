fc1c6d96463dd0907a364f1a686e15b0e1fd5f48
alee <alessandrdm@gmail.com>
2025-05-08 19:59:56 -0400

    zip

--- zip.py
+++ zip.py
@@ -1,3 +1,4 @@
+"""
 gabarito = [
     'a', 'b', 'c', 'd', 'e',
     'a', 'b', 'c', 'd', 'e',
@@ -6,8 +7,23 @@ gabarito = [
 ]
 
 total = len(gabarito)
-respostas = ['a', 'b', 'e', 'b', 'd', 'a', 'e', 'c', 'b', 'd', 'a', 'e', 'c', 'b', 'd', 'a', 'e', 'c', 'b', 'd']
+"""
+minha_resposta = ['a', 'b', 'e', 'b', 'd', 'a', 'e', 'c', 'b', 'd', 'a', 'e', 'c', 'b', 'd', 'a', 'e', 'c', 'b', 'd']
 
+"""
 acertos = sum(1 for c, t in zip(gabarito, respostas)if c.lower() == t.lower())
-
 print(f"{acertos}/20")
+"""
+
+#if valor1 == valor2
+ #   print(f"{valor}/{total}")
+
+def resposta_correta(minha_resposta):
+    gabarito = ['a', 'b', 'e', 'b', 'd', 'a', 'e', 'c', 'b', 'd', 'a', 'e', 'c', 'b', 'd', 'a', 'e', 'c', 'b', 'd']
+    total = len(gabarito)
+    valor = sum(1 for val1, val2 in zip(gabarito, minha_resposta) if val1 == val2)
+    porcentagem = (valor / total) * 100
+    return f"{valor}/{total} = {porcentagem}%"
+
+print(resposta_correta(minha_resposta))
+
